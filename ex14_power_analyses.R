3083a5a1937c5920fec54a6831f66bbb1e848fd5
klangwig <klangwig@gmail.com>
2018-04-26 13:39:47 -0400

    short power analysis example

---
+++ ex14_power_analyses.R
@@ -0,0 +1,15 @@
+library(pwr)
+pwr.t.test(d = 0.8, sig.level =0.05, power =.8 )
+#d is effect size, and power is how certain you want to be
+pwr.t.test(d = 0.5, sig.level =0.05, power =.8 )
+#if you think the effect is small, you need to be more certain
+#we basically just make up these numbers because we don't really know
+
+#we can also change the parameters to see how underpowered we will be
+#given realistic sample sizes
+pwr.t.test(n=10,d = 0.5, sig.level =0.05)
+#this isn't great - we have pretty small power
+pwr.t.test(n=20,d = 0.5, sig.level =0.05)
+
+##see this for more info
+#https://www.statmethods.net/stats/power.html
