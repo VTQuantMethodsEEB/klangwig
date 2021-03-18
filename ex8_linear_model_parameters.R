28d75996c777391a5cc27d31ed7f9311a7a3ac15
klangwig <klangwig@gmail.com>
2021-03-18 14:41:15 -0400

    current update with continuous bins

--- ex8_linear_model_parameters.R
+++ ex8_linear_model_parameters.R
@@ -65,11 +65,18 @@ pr(lm1 <- lm(colonies~place,data=ants))
 library(effects)
 summary(allEffects(lm1))
 
-#lsmeans
+#emmeans
 library(emmeans)
-emmeans(lm1,specs = ~place)
-#this is still a useful vignette:
-#https://cran.r-project.org/web/packages/lsmeans/vignettes/using-lsmeans.pdf
+em_lm1 = emmeans(lm1,specs = ~place)
+em_lm1
+plot(em_lm1)
+#this is a useful vignette:
+#https://cran.r-project.org/web/packages/emmeans/vignettes/comparisons.html
+
+
+#emmeans
+em_lm3 = emmeans(lm3,specs = ~place*observers)
+em_lm3
 
 #plot the effects
 plot(allEffects(lm1))
@@ -155,6 +162,8 @@ cld(lsm1$emmeans, Letters = "ABCDEFGHIJ")
 
 ## Interactive Models ##
 pr(lmTL2 <- lm(grahami~time*light,data=lizards))
+lmTL2 <- lm(grahami~time*light,data=lizards)
+summary(lmTL2)
 
 ## Releveling interactive models
 lizards$time <- relevel(lizards$time, ref = "midday" )
@@ -165,27 +174,91 @@ pr(lmTL2 <- lm(grahami~time*light,data=lizards))
 lsm2<-emmeans(lmTL2,pairwise~time*light)
 lsm2
 
+library(effects)
+plot(allEffects(lmTL2))
+
 ##plotting the interactive model
+lizards$yhat = predict(lmTL2)
 
+##plotting the interactive model
 pp <- with(lizards,
-           expand.grid(time=levels(time),light=levels(light)))
+           expand.grid(time=levels(time),
+                       light=levels(light)))
+#make a new dataframe with all the unique values of time
+#and all the unique values light
+#expand.grid says "give me every combination possible"
 
-pp2 <- pp
-pp2$grahami <- predict(lmTL2,newdata=pp2)
+pp
+#look at new data frame
+
+pp$grahami <- predict(lmTL2,newdata=pp)
+#this says- predict the number of grahami lizards using my new data frame, pp
+#since pp has every combination possible, you will get a prediction for every value
 
 
-ggplot(pp2,aes(x=time,y=grahami,colour=light))+
+ggplot(pp,aes(x=time,y=grahami,colour=light))+
   geom_point()+
   geom_line(aes(group=light))
+#this is a plot of our model predictions
+#but we need to add our actual data
 
 ###ADD RAW data to plot###
-###ALWAYS ADD RAW DATA!###
-ggplot(pp2,aes(x=time,y=grahami,colour=light))+ #pp2 is the dataframe with our predictions
-  geom_point(size=4)+ #the large solid points show our predictions
-  geom_line(aes(group=light))+ #the lines connect our predictions
-  geom_point(data=lizards, aes(x=time,y=grahami,colour = light), shape=1) #this shows our RAW data
-  #lizards contains our raw data frame and we plot that on top on the predictions in open circles
-  # we specify open circles with "shape = 1"
+ggplot(pp,aes(x=time,y=grahami,colour=light))+ #set up plot using predictions dataset
+  geom_point()+ #plot the prediction
+  geom_line(aes(group=light))+ #draw lines b/t predictions, group them by light conditions
+  geom_point(data=lizards, aes(x=time,y=grahami,colour = light)) #add the observed data to the plot
+#note - I am calling the original dataframe - lizards
+#i am therefore setting up aes again
+
+
+##what if I had a continuous variable?
+lmLC <- lm(grahami~time*opalinus, data=lizards)
+summary(lmLC)
+
+#make a new dataframe
+new.dat.combos <- with(lizards, #use lizards
+                       expand.grid(time=levels(time), #give me all the unique values of time
+                                   opalinus=seq(min(opalinus),max(opalinus), by=1)
+                                   #give me a sequence of numbers from the min of opalinus to the max of opalinus, space the numbers "by 1"
+                       ))
+
+#predict number of grahami using new data frame
+new.dat.combos$grahami <- predict(lmLC,newdata=new.dat.combos)
+
+
+###plotting prediction + data with continuous example#
+ggplot(new.dat.combos,aes(x=opalinus,y=grahami,colour=time))+ #set up plot using predictions dataset
+  geom_line(aes(group=time))+ #draw lines that are predictions, group them by light conditions
+  geom_point(data=lizards, aes(x=opalinus,y=grahami,colour = time)) #add the observed data to the plot
+#note - I am calling the original dataframe - lizards
+#i am therefore setting up aes again
+
+##if you have two continuous variables, you will prob want to bin to ease visualization##
+bats = read.csv("bat_data.csv")
+head(bats)
+bats$lgdL = log10(bats$gdL)
+
+lmBAT <- lm(lgdL~count*temp, data= bats)
+summary(lmBAT)
+plot(allEffects(lmBAT))
+#this knows to bin the temperature data
+
+new.bat.combos = with(bats,
+                      expand.grid(
+                        count = seq(min(count, na.rm=T), max(count, na.rm=T), by=1),
+                        temp = c(2, 6, 10) #just pick 3 temps to demonstrate effect
+                      ))
+
+
+#predict lgdL using new data frame - preserve original predictor structure (e.g. use temp, not temp.bin)
+new.bat.combos$lgdL <- predict(lmBAT,newdata=new.bat.combos)
+
+
+###plotting prediction + data with two continuous variables, binning one#
+ggplot(new.bat.combos,aes(x=count,y=lgdL,colour=temp))+ #set up plot using predictions dataset
+  geom_line(aes(group=temp))+ #draw lines that are predictions, group them by light conditions
+  geom_point(data=bats, aes(x=count,y=lgdL,colour = temp)) #add the observed data to the plot
+
 
 ##more graphics
 
@@ -195,7 +268,13 @@ lizards <- mutate(lizards,
 
 pr(lmTL2 <- lm(grahami~time*light,data=lizards))
 
+library(dotwhisker)
+
+dwplot(list(additive=lmTL1,interaction=lmTL2))+
+  geom_vline(xintercept=0,lty=2)
+
 plot(allEffects(lmTL2))
+
 
 
 
